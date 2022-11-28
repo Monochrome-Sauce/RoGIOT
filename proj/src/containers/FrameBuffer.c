4c7b1fe690b7e4e1734630c470dcffede831841a
Ronen A <dr.robostorm@gmail.com>
2022-11-28 13:44:57 +0200

    Implement a frame buffer container

---
+++ proj/src/containers/FrameBuffer.c
@@ -0,0 +1,66 @@
+#include "FrameBuffer.h"
+#include "src/macros.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+static uint32_t inner__calc_size(const uint16_t x, const uint16_t y)
+{
+	return (uint32_t)x * (uint32_t)y;
+}
+
+bool FrameBuffer__init(struct FrameBuffer *const buff, const uint16_t width, const uint16_t height)
+{
+	assert(buff != NULL);
+	assert(width > 0 && height > 0);
+	
+	buff->width = width;
+	buff->height = height;
+	buff->capacity = 0;
+	
+	const uint32_t capacity = inner__calc_size(width, height);
+	buff->data = rgt_newarr(capacity, FrameBuffer_DataType);
+	
+	if likely (buff->data != NULL) {
+		memset(buff->data, ' ', capacity * sizeof (FrameBuffer_DataType));
+		buff->capacity = capacity;
+	}
+	return buff->data != NULL;
+}
+
+void FrameBuffer__free(struct FrameBuffer *buff)
+{
+	rgt_free(buff);
+}
+
+bool FrameBuffer__resize(struct FrameBuffer *const buff, const uint16_t width, const uint16_t height)
+{
+	assert(buff != NULL && buff->data != NULL);
+	assert(width > 0 && height > 0);
+	
+	const uint32_t newCapacity = inner__calc_size(width, height);
+	if (newCapacity > buff->capacity) {
+		void *tmp = realloc(buff->data, newCapacity * sizeof (FrameBuffer_DataType));
+		
+		if unlikely (tmp == NULL) {
+			return false;
+		}
+		buff->data = tmp;
+		buff->capacity = newCapacity;
+	}
+	
+	buff->width = width;
+	buff->height = height;
+	return true;
+}
+
+FrameBuffer_DataType* FrameBuffer__at(struct FrameBuffer *const buff,
+                                      const uint16_t x, const uint16_t y)
+{
+	assert(buff != NULL && buff->data != NULL);
+	assert(x < buff->width && y < buff->height);
+	
+	return &(*buff->data)[y * buff->width + x];
+}
---
+++ proj/src/containers/FrameBuffer.h
@@ -0,0 +1,44 @@
+#ifndef CONTAINERS__FRAME_BUFFER_H
+#define CONTAINERS__FRAME_BUFFER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+
+typedef char FrameBuffer_DataType;
+struct FrameBuffer
+{
+	uint32_t capacity; /* the real amount of allocated data members */
+	uint16_t width, height; /* imaginary xy dimensions for access */
+	FrameBuffer_DataType (*data)[];
+};
+
+
+/* #Initialize a the buffer's data and sets all members to a space character.
+! @param buff: pointer to a FrameBuffer.
+! @return: false on failure to initiate the data buffer, otherwise true. When false is
+returned, the width and height will be initiated to 0.
+*/
+bool FrameBuffer__init(struct FrameBuffer *const buff, uint16_t width, uint16_t height);
+
+/* #Free an object's data.
+! @param buff: pointer to a FrameBuffer.
+*/
+void FrameBuffer__free(struct FrameBuffer *buff);
+
+/* #Resize an existing object's data.
+! @param buff: pointer to a FrameBuffer.
+! @return: false on failure to resize, otherwise true.
+*/
+bool FrameBuffer__resize(struct FrameBuffer *buff, uint16_t width, uint16_t height);
+
+/* #Get pointer to an element of the buffer.
+! @param buff: pointer to a FrameBuffer.
+! @param x: X position of element. X must be less than the width of the buffer.
+! @param y: Y position of element. Y must be less than the height of the buffer.
+! @return: pointer to the element of the buffer at position (y,x).
+*/
+FrameBuffer_DataType* FrameBuffer__at(struct FrameBuffer *buff, uint16_t x, uint16_t y);
+
+
+#endif /* CONTAINERS__FRAME_BUFFER_H */
